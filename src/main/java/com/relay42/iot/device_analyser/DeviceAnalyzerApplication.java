19308cba9903eb6a6f8cb6a153578c16ed71be8f
shalinda-2019563 <shalinda.2019563@iit.ac.lk>
2022-03-12 11:25:16 +0530

    Init  project
    Initialize project using springboot framework

---
+++ src/main/java/com/relay42/iot/device_analyser/DeviceAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.relay42.iot.device_analyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relay42/iot/device_analyser/DeviceAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relay42.iot.device_analyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
