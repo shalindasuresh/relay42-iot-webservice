e24a1471651228ee8d144489778f874b1a478e2e
shalinda-2019563 <shalinda.2019563@iit.ac.lk>
2022-03-21 06:55:48 +0530

    Add cassandra custom querries

--- src/main/java/com/relay42/iot/device_analyser/controller/DeviceController.java
+++ src/main/java/com/relay42/iot/device_analyser/controller/DeviceController.java
@@ -1,13 +1,36 @@
 package com.relay42.iot.device_analyser.controller;
 
+import com.relay42.iot.device_analyser.model.DeviceRead;
 import com.relay42.iot.device_analyser.repository.DeviceReadRepository;
+import com.relay42.iot.device_analyser.request.DeviceReadingRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 class DeviceController {
 
-   
+    @Autowired
+    DeviceReadRepository deviceReadRepository;
+
+    @PostMapping("/devices/query/type")
+    public void queryDeviceReadingByType(@Valid @RequestBody DeviceReadingRequest request){
+
+        try {
+           List<DeviceRead> deviceRead=deviceReadRepository.findBasicStatsByDeviceType(request.getType());
+
+            System.out.println("************************************");
+            System.out.println(deviceRead.get(0).getName());
+            System.out.println(deviceRead.get(0).getValue());
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
 
 }
--- src/main/java/com/relay42/iot/device_analyser/repository/DeviceReadRepository.java
+++ src/main/java/com/relay42/iot/device_analyser/repository/DeviceReadRepository.java
@@ -1,13 +1,24 @@
 package com.relay42.iot.device_analyser.repository;
 
-import java.util.UUID;
-
-
 import com.relay42.iot.device_analyser.model.DeviceRead;
+import org.springframework.data.cassandra.repository.AllowFiltering;
+import org.springframework.data.cassandra.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface DeviceReadRepository extends CrudRepository<DeviceRead, UUID> {
 
-    DeviceRead findByType(String name);
+    DeviceRead findByType(String type);
+
+    @AllowFiltering
+    @Query(value = "SELECT AVG(value),MIN(value),MAX(value),name  FROM device_read WHERE type='HUMIDITY' ")
+    List<DeviceRead> findBasicStatsByDeviceType(String type);
+
+    @AllowFiltering
+    @Query(value = "SELECT AVG(value),MIN(value),MAX(value),name  FROM device_read WHERE name='HUMIDITY' ")
+    List<DeviceRead> findBasicStatsByDeviceName(String name);
+
 
 }
